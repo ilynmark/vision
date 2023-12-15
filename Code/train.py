8f6ea17a636f1bf5e8ec708d875023b518773924
ilynmark <mar.miskaryan@gmail.com>
2023-12-16 01:54:54 +0400

    Update file

--- Code/train.py
+++ Code/train.py
@@ -1,11 +1,20 @@
 # Contains the training codes. It must accept a dataset path and hyperparameters as inputs. It should produce and save at least one checkpoint as output.
 
 import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 from dataset import *
 from inference import *
 
 train_batches, test_batches, info, sample_image, sample_mask = get_dataset()
+
+# Define a ModelCheckpoint callback
+checkpoint_path = 'model_checkpoint.h5'
+checkpoint_callback = ModelCheckpoint(checkpoint_path, 
+                                      save_best_only=True,
+                                      monitor='val_accuracy',
+                                      mode='max',
+                                      verbose=1)
 
 def train(epochs=20, val_subsplits=5, batch_size=64):
     val_steps = info.splits['test'].num_examples//batch_size//val_subsplits
@@ -17,7 +26,7 @@ def train(epochs=20, val_subsplits=5, batch_size=64):
                               steps_per_epoch=steps_per_epoch,
                               validation_steps=val_steps,
                               validation_data=test_batches,
-                              callbacks=[DisplayCallback()])
+                              callbacks=[DisplayCallback(), checkpoint_callback])
     return model_history
 
 def visualize_loss(model_history):
