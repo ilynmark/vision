9f6ec9dba45d6a571ded3f987f8277ff2a59caf0
ilynmark <mar.miskaryan@gmail.com>
2023-12-16 00:09:39 +0400

    Update file

--- Code/inference.py
+++ Code/inference.py
@@ -28,6 +28,35 @@ def show_predictions(dataset=None, num=1, sample_image=sample_image, sample_mask
     else:
         display([sample_image, sample_mask, create_mask(model.predict(sample_image[tf.newaxis, ...]))])
             
+def make_predictions(test_dataset=test_batches, output_dir='../Result', num=5):
+    os.makedirs(output_dir, exist_ok=True)
+
+    for i, (image, mask) in enumerate(test_dataset.take(num)):
+        pred_mask = model.predict(image)
+        pred_mask = create_mask(pred_mask)
+
+        # Create output folders
+        save_path = os.path.join(output_dir, f"prediction_{i+1}")
+        os.makedirs(save_path, exist_ok=True)
+
+        # Save the images to the output directory
+        plt.imshow(image[0].numpy())
+        plt.axis('off')
+        plt.savefig(os.path.join(save_path, f"input_image_{i+1}.png"))
+        plt.close()
+        
+        # Save the mask using plt
+        plt.imshow(mask[0].numpy().squeeze())  # assuming mask is grayscale
+        plt.axis('off')
+        plt.savefig(os.path.join(save_path, f"label_mask_{i+1}.png"))
+        plt.close()
+
+        # Save the predicted mask using plt
+        plt.imshow(pred_mask.numpy().squeeze())  # assuming pred_mask is grayscale
+        plt.axis('off')
+        plt.savefig(os.path.join(save_path, f"predicted_mask_{i+1}.png"))
+        plt.close()
+    
 class DisplayCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         clear_output(wait=True)
