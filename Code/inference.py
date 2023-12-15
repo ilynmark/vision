f93af7ece0ac1cb93c7a745a7759822a0e00f10d
ilynmark <mar.miskaryan@gmail.com>
2023-12-15 21:37:58 +0400

    Add Dataset, Model, Train and Inference code files and Segmentation notebook

--- Code/inference.py
+++ Code/inference.py
@@ -1,2 +1,35 @@
 # Contains the model inference code for processing a single image.
 # It should take a single image path as input and save the output on Result folder.
+
+import tensorflow as tf
+import os
+from IPython.display import clear_output
+from model import *
+from dataset import *
+
+output_classes=3
+model = unet_model(output_channels=output_classes)
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+train_batches, test_batches, info, sample_image, sample_mask = get_dataset()
+
+def create_mask(pred_mask):
+    pred_mask = tf.math.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+
+def show_predictions(dataset=None, num=1, sample_image=sample_image, sample_mask=sample_mask):
+    if dataset:
+        for image, mask in dataset.take(num):
+            pred_mask = model.predict(image)
+            display([image[0], mask[0], create_mask(pred_mask)])
+    else:
+        display([sample_image, sample_mask, create_mask(model.predict(sample_image[tf.newaxis, ...]))])
+            
+class DisplayCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        clear_output(wait=True)
+        show_predictions(sample_image=sample_image, sample_mask=sample_mask)
+        print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
--- Code/train.py
+++ Code/train.py
@@ -1,1 +1,35 @@
 # Contains the training codes. It must accept a dataset path and hyperparameters as inputs. It should produce and save at least one checkpoint as output.
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from dataset import *
+from inference import *
+
+train_batches, test_batches, info, sample_image, sample_mask = get_dataset()
+
+def train(epochs=20, val_subsplits=5, batch_size=64):
+    val_steps = info.splits['test'].num_examples//batch_size//val_subsplits
+    
+    train_length = info.splits['train'].num_examples
+    steps_per_epoch = train_length // batch_size
+
+    model_history = model.fit(train_batches, epochs=epochs,
+                              steps_per_epoch=steps_per_epoch,
+                              validation_steps=val_steps,
+                              validation_data=test_batches,
+                              callbacks=[DisplayCallback()])
+    return model_history
+
+def visualize_loss(model_history):
+    loss = model_history.history['loss']
+    val_loss = model_history.history['val_loss']
+
+    plt.figure()
+    plt.plot(model_history.epoch, loss, 'r', label='Training loss')
+    plt.plot(model_history.epoch, val_loss, 'bo', label='Validation loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss Value')
+    plt.ylim([0, 1])
+    plt.legend()
+    plt.show()
